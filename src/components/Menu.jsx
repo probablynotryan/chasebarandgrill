635b93521f698744e8d6f71cbef9ea5017bdabcb
Ryan Bass <theryanbass@gmail.com>
2023-01-14 12:14:10 -0800

    add menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+// import capacitor from '../img/capacitor.jpg';
+// import westand from '../img/we_stand.jpg';
+// import mountedtv from '../img/mounted_tv.webp';
+
+export default function Menu() {
+  return (
+    <>
+      <div className="py-14 text-center">
+        Menus go here, either as pictures or as text.
+      </div>
+    </>
+  );
+}
